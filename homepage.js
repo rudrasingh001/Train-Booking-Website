514d038390c85a3f6f1b20d698307a36ef37bf70
RUDRA PRATAP SINGH <88509277+rudrasingh001@users.noreply.github.com>
2022-11-22 02:06:36 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,9 @@
+function swapValues(){
+var tmp = document.getElementById("c").value;
+document.getElementById("c").value = document.getElementById("d").value;
+document.getElementById("d").value = tmp;
+    
+}
+
+
+
